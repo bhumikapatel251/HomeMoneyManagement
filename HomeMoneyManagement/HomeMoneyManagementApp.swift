9aba4e9cccf7f35b7c0de08d287eb6fec2f6ebf8
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-24 13:52:28 +0530

    Initial Commit

---
+++ HomeMoneyManagement/HomeMoneyManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeMoneyManagementApp.swift
+//  HomeMoneyManagement
+//
+//  Created by Bhumika Patel on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeMoneyManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
