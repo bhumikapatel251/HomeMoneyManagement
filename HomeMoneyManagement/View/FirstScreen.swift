567130aa8ac2fde6df7ea68b4e116491e29a1fa8
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-25 11:44:12 +0530

    Add plus button in first screen

--- HomeMoneyManagement/View/FirstScreen.swift
+++ HomeMoneyManagement/View/FirstScreen.swift
@@ -10,15 +10,23 @@ import SwiftUI
 struct FirstScreen: View {
     var body: some View {
         ZStack{
-           Color.black
+           Color("LightBlack")
                 .ignoresSafeArea()
             ScrollView(.vertical, showsIndicators: false) {
                 HStack{
                     Button{
                         
                     } label: {
+                        Image(systemName: "book")
+                            .foregroundColor(.white)
+                            
+                    }
+                    Button{
+                        
+                    } label: {
                         Image(systemName: "magnifyingglass")
                             .foregroundColor(.white)
+                            
                     }
                     Button{
                         
@@ -28,6 +36,7 @@ struct FirstScreen: View {
                     }
 
                 }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
                 .padding()
                 ScrollView(.vertical, showsIndicators: false){
                     HStack(spacing:18){
@@ -35,7 +44,25 @@ struct FirstScreen: View {
                             TaskTypeView(type: type)
                         }
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                    .padding(.horizontal,30)
                 }
+                Button{
+                    
+                } label: {
+                    
+                    Image(systemName: "plus")
+                        .foregroundColor(.white)
+                        .frame(width: 30, height: 30)
+                        .background(RadialGradient(colors: [.pink,.red,.yellow], center: .center, startRadius: 1, endRadius: 60))
+                            .clipShape(Circle())
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
+                            .padding(.vertical,600)
+                        
+                }
+                
+                .padding(30)
+                
             }
         }
     }
