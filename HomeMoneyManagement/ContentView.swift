15061a482e5a3b72033c5e608205e66db944b086
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-26 14:02:35 +0530

    create add and slider horizontal button and back action worked for set environment value and throw action

--- HomeMoneyManagement/ContentView.swift
+++ HomeMoneyManagement/ContentView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isActive = false
+    @State private var size = 1.2
+    @State private var opacity = 0.8
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            if isActive{
+                FirstScreen()
+                    .navigationBarHidden(true)
+            }else{
+                VStack{
+                    VStack(alignment:.trailing){
+                        Image("Home2")
+                            .resizable()
+                            .frame(width: 120, height: 120, alignment: .trailing)
+                            .font(.system(size: 80))
+                        Text("HomeMoneyManagement")
+                            .font(.system(size: 10))
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color("BG1"))
+                    
+                        
+                    }
+                    .padding(.bottom,50)
+                    .scaleEffect(size)
+                    .opacity(opacity)
+                    .onAppear{
+                        withAnimation(.easeIn(duration: 1.2)) {
+                            self.size = 2.0
+                            self.opacity = 2.8
+                        }
+                    }
+                
+                }
+                .onAppear{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                        self.isActive = true
+                    }
+                }
+       
+        
+            }
+        }
+        
     }
+        
+      
 }
 
 struct ContentView_Previews: PreviewProvider {
--- HomeMoneyManagement/View/filteredDetailView.swift
+++ HomeMoneyManagement/View/filteredDetailView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct FilteredDetailView: View {
     @EnvironmentObject var expenseViewModel: ExpenseViewModel
+    
+    // environment Value set for backButton
+    @Environment(\.self) var env
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false){
-                
+            VStack(spacing: 15){
+                HStack(spacing:15){
+                    // Back Button
+                    
+                    Button{
+                        env.dismiss()
+                    } label: {
+                        Image(systemName: "arrow.backward.circle.fill")
+                            .foregroundColor(.gray)
+    //                               .overlay(content: {
+    //                                   Circle()
+    //                                       .stroke(.white,lineWidth: 2)
+    //                                       .padding(7)
+    //                               })
+                            .frame(width: 30, height: 30)
+                            .background(Color.white,in: RoundedRectangle(cornerRadius: 10, style: .continuous))
+                            .shadow(color: .black.opacity(0.1), radius: 5, x: 5, y: 5)
+                    }
+                    VStack(alignment: .leading, spacing: 4){
+                        Text("Transactions")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .opacity(0.7)
+                            .frame(maxWidth: .infinity,  alignment: .leading)
+                            
+                    }
+                    // slider horizontal line symbol
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
+                            .foregroundColor(.gray)
+                            .frame(width: 30, height: 30)
+                            .background(Color.white,in: RoundedRectangle(cornerRadius: 10, style: .continuous))
+                            .shadow(color: .black.opacity(0.1), radius: 5, x: 5, y: 5)
+                    }
+                }
+            }
+            .padding()
         }
         .navigationBarHidden(true)
         .background{
-            Color("LightBlack")
+            Color("BG")
                 .ignoresSafeArea()
         }
     }
